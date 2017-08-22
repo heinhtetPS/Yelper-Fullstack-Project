36d6308e6afbac0c8562d058a9a1c08ad4f77836
heinhtetPS <heinhtet.pyisoe@gmail.com>
2017-08-22 10:30:12 -0400

    Complete user model minus associations pending until other tables migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,46 @@ class User < ApplicationRecord
   attr_reader :password
   after_initialize :ensure_session_token
 
-  #Do ASSOCS here 
+  #Do ASSOCS here when other tables are migrated
+
+  #Used to find user object in DB
+  def self.find_by_credentials(username, raw_pass)
+    user = User.find_by(name: username)
+    if user && user.is_password?(raw_pass)
+      return user
+    else
+      return nil
+    end
+  end
+
+  #Required for auth session, generates random token
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  #used to refresh session
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  #used to check raw pass against salted/hashed digest
+  def is_password?(raw_pass)
+    BCrypt::Password.new(self.password_digest).is_password?(raw_pass)
+  end
+
+  #Used to generate salted/hashed digest
+  def password=(raw_pass)
+    @password = password
+    self.password_digest = BCrypt::Password.create(raw_pass)
+  end
 
 
-
+  private
+  #used to make sure session_token exists
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 
 end
