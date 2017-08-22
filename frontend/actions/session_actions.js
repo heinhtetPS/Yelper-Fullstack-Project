8b6761beb507385dcfdb5f0c6ac8cf5982c8678d
heinhtetPS <heinhtet.pyisoe@gmail.com>
2017-08-22 15:20:28 -0400

    write session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,36 @@
+import * as sessionUtil from './util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const createNewUser = user => dispatch => (
+  sessionUtil.createNewUser(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const loginUser = user => dispatch => (
+  sessionUtil.loginUser(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logoutUser = () => dispatch => (
+  sessionUtil.logoutUser().then(user => (
+    dispatch(receiveCurrentUser(null))
+  ))
+);
