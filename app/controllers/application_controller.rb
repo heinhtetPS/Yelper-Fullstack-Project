a089a56d744993c3fa37ae3841777a0122ea5ddc
heinhtetPS <heinhtet.pyisoe@gmail.com>
2017-08-22 10:39:34 -0400

    Write auth helper methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #used to check global session state
+  helper_method :current_user, :logged_in?
+
+  private
+
+  #enables user checks inside html or js views
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  #checks that current user is NOT null
+  def logged_in?
+    !!current_user
+  end
+
+  #log the user in, generating a session_token and setting as current
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  #destroy session token upon logout
+  def logout()
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  #redirects user from forbidden content
+  def require_login!
+    redirect_to new_session_url unless logged_in?
+  end
+
 end
