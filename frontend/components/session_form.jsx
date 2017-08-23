42e8bb7c0e707afc6b3928977c278e6f40181c7b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-08-23 11:02:35 -0400

    add functionality to toggle between sign up and login forms

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SessionForm extends React.Component {
 
@@ -20,24 +21,41 @@ class SessionForm extends React.Component {
  whichform() {
    if (this.props.location.pathname === "/signup") {
      return(
-       <form>
-         <input type ="text" placeholder="Username"></input> <br />
-         <input type ="text" placeholder="Email"></input> <br />
-         <input type ="text" placeholder="Password"></input> <br />
-       </form>
+       <div>
+         <form>
+           <h2>Sign Up for Yelp</h2> <br />
+           <p>Connect with great local businesses</p> <br />
+           <p>By signing up, you agree to Yelp’s Terms of Service and Privacy Policy.</p>
+           <input type ="text" placeholder="Username"></input> <br />
+           <input type ="text" placeholder="Email"></input> <br />
+           <input type ="text" placeholder="Password"></input> <br />
+           <button>Sign up</button> <br />
+           <p className="tinytext">Already on Yelp?</p>
+           <Link to="/login">Log In</Link>
+         </form>
+         <img className="right-side-house" src="https://s3-media4.fl.yelpcdn.com/assets/2/www/img/1e82406ff345/signup/signup_illustration.png"></img>
+       </div>
      );
    } else {
      return (
-       <form>
-         <input type ="text" placeholder="Username"></input> <br />
-         <input type ="text" placeholder="Password"></input> <br />
-       </form>
+       <div>
+         <form>
+           <h2>Log In to Yelp</h2> <br />
+           <p>New to Yelp?</p> <Link to="/signup">Sign up!</Link> <br />
+           <p>By signing up, you agree to Yelp’s Terms of Service and Privacy Policy.</p>
+           <input type ="text" placeholder="Username"></input> <br />
+           <input type ="text" placeholder="Password"></input> <br />
+           <button>Log In</button> <br />
+           <p className="tinytext">New to Yelp?</p>
+           <Link to="/signup">Sign up!</Link>
+         </form>
+         <img className="right-side-house" src="https://s3-media4.fl.yelpcdn.com/assets/2/www/img/1e82406ff345/signup/signup_illustration.png"></img>
+       </div>
      );
    }
  }
 
  render() {
-   debugger
    return (
      <div>
        {this.whichform()}
