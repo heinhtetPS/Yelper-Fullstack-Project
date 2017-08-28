a593bc6274207c68cf8fe9b557b30ad2708d1c9f
heinhtetPS <heinhtet.pyisoe@gmail.com>
2017-08-28 18:06:10 -0400

    fix seeds for heroku to reset db properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
 User.create!({username: 'demo_user',
               email: 'demo@gmail.com',
               password: 'password'})
