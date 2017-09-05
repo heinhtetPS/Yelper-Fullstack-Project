a22cd4ffe5a37c0dcac591f1abef22a81c2198db
Hein Htet Pyi Soe <heinhtet.pyisoe@gmail.com>
2017-09-05 00:21:17 -0400

    add better styling to nav and search bars

--- frontend/components/biz/search.jsx
+++ frontend/components/biz/search.jsx
@@ -47,27 +47,35 @@ class Search extends React.Component {
 
    if (this.props.match.path === "/") {
      searchcontainer = "search-container-splash";
+     searchheader = "search-header";
+     searchkeyword = "search-keyword";
+     searchlocation = "search-location";
+     searchsubmit = "search-submit-home";
    } else {
      searchcontainer = "search-container-top";
+     searchheader = "search-header-top";
+     searchkeyword = "search-keyword-top";
+     searchlocation = "search-location-top";
+     searchsubmit = "search-submit-top";
    }
 
    return (
      <div>
        <div className={searchcontainer}>
          <form className="search_form"  onSubmit={this.handleSubmit}>
-           <span className="search-header">
+           <span className={searchheader}>
              Find
            </span>
-           <input className="search-keyword"type="text"
+           <input className={searchkeyword}type="text"
              placeholder="tacos, cheap dinner, Max's"
               onChange={this.update('categories')}
 
              />
-           <input className="search-location"type="text"
+           <input className={searchlocation}type="text"
                placeholder="New York, NY"
               onChange={this.update('bounds')}
                />
-             <button className="search-submit-home" onSubmit={this.handleSubmit}>
+             <button className={searchsubmit} onSubmit={this.handleSubmit}>
                <span>
                  <svg className="svg-search">
                    <path d="M20.753 19.34l-4.295-4.297A7.46 7.46 0 0 0 18 10.5a7.5 7.5 0 1 0-7.5 7.5 7.46 7.46 0 0 0 4.543-1.542l4.296 4.295a1 1 0 1 0 1.412-1.414zM10.5 16A5.506 5.506 0 0 1 5 10.5C5 7.467 7.467 5 10.5 5S16 7.467 16 10.5 13.533 16 10.5 16z"></path>
