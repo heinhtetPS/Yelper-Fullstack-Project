dea3dd28d58ca73bd0feaeea6ee4ddaf948cf8a5
Hein Htet Pyi Soe <heinhtet.pyisoe@gmail.com>
2017-09-19 18:16:07 -0400

    new old commit before minification chaos

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -2,6 +2,10 @@ import values from 'lodash/values';
 
 export const selectAllbiz = biz => values(biz);
 
+export const selectThreeRandomBiz = biz => {
+
+};
+
 export const asArray = ({ filters }) => {
   if (filters === null || filters === undefined)
   return [];
