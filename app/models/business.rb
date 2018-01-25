da27d4e1b87e69313f7fc1fc91d3bde1947d7ff4
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-01-24 23:18:11 -0500

    include pgsearch gem and updates

--- app/models/business.rb
+++ app/models/business.rb
@@ -22,6 +22,7 @@
 #
 
 class Business < ApplicationRecord
+  include pgSearch
   validates :name, :map_lat, :map_lng, :address, :price, presence: true
   validates :categories, presence: { message: 'business must belong to at least one category'}
   validates :price, inclusion: { in: (1..4) }
