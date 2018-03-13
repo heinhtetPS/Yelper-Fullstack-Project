f72dc5cbb26f02d33ccf56c9dd09427cf1247906
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-03-12 20:13:09 -0400

    add filter buttons back for experimentation

--- frontend/components/biz/results_show.jsx
+++ frontend/components/biz/results_show.jsx
@@ -15,7 +15,15 @@ class ResultsShow extends React.Component {
       this.props.updateFilter("searchterm", this.props.location.search.slice(1));
   }
 
+
+
+
+
   toggleSelected() {
+
+  }
+
+  toggleDollars() {
 
   }
 
@@ -24,11 +32,55 @@ class ResultsShow extends React.Component {
     let searchterm = this.props.location.search.slice(1);
     let msgbox = this.props.businesses.length === 0 ? "" : "hidden";
 
+    //vars for filter button logic:
+    // dollar toggle has 4 buttons: $ $$ $$$ $$$$
+    // remove 1 if another is clicked, only 1 can be picked at a time
+    // 3 single toggles: open now, Delivery, Takeout
+    const dollarFilter = [false, false, false, false]
+    const openNow = false;
+    const delivery = false;
+    const takeOut = false;
+
     return (
       <div className="results-page-container">
         <div className="top-shelf-options">
           <div className="top-shelf-content">
             <h2>Best {searchterm}(s) in New York, NY</h2>
+
+              <ul className="filters-row">
+
+                  <li className="filters-element1">
+                    <ul className="dollars-container">
+
+                      <li className="dollars-button-unselected">$
+                        <input type="checkbox" className="hidden-checkbox" value="1" /></li>
+
+                      <li className="dollars-button-unselected">$$
+                      <input type="checkbox" className="hidden-checkbox" value="2" /></li>
+
+                      <li className="dollars-button-unselected">$$$
+                      <input type="checkbox" className="hidden-checkbox" value="3" /></li>
+
+                      <li className="dollars-button-unselected">$$$$
+                      <input type="checkbox" className="hidden-checkbox" value="4" /></li>
+                    </ul>
+                  </li>
+
+                  <li className="filters-element2">
+                    <label className="open-button">Open Now</label>
+                    <input type="checkbox" className="hidden-checkbox" value="selected" />
+                  </li>
+
+                  <li className="filters-element2">
+                    <label className="delivery-button">Delivery</label>
+                    <input type="checkbox" className="hidden-checkbox" value="unselected" />
+                  </li>
+
+                  <li className="filters-element2">
+                    <label className="takeout-button">Take-out</label>
+                    <input type="checkbox" className="hidden-checkbox" value="unselected" />
+                  </li>
+              </ul>
 
           </div>
         </div>
